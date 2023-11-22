8bc7717d0b5759cc3200d14f21bb35f49eaad63a
luna-spr <luna@spreatics.com>
2023-11-22 14:01:33 +0900

    express, js 심화

--- 07-3-express/app.js
+++ 07-3-express/app.js
@@ -9,8 +9,8 @@ app.set('views', './views');
 
 // static 미들웨어 등록 (정적 파일 로드 ex. css, js)
 // static 라는 실제 폴더를 static 이름으로 접근하겠다
-app.use('/static', express.static(__dirname + '/static'));
-console.log(__dirname); // ~~/07-3-express
+app.use('/public', express.static(__dirname + '/static'));
+// console.log(__dirname); // ~~/07-3-express
 
 // app.get(경로, 해당 경로로 들어왔을 때 실행할 함수)
 // localhost:8000/ 경로로 접속했을 때
@@ -19,8 +19,26 @@ app.get('/', function (req, res) {
     // res.send('<h1>Hello Express!</h1>');
 
     // index라는 파일명을 찾아서 해당 파일 렌더
-    res.render("index");
+    res.render("index", {
+        btns: ['사과', '오렌지', '키위'],
+        isLogin: true,
+        me: {
+            name: 'Luna',
+            msg: '반갑습니다~'
+        }
+    });
 })
+
+// /login 경로로 접속했을 때
+app.get('/login', function (req, res) {
+    res.render('login');
+})
+
+// /register 경로로 접속했을 때
+app.get('/register', function (req, res) {
+    res.render('register');
+})
+
 
 app.get('/kdt', function (req, res) {
     // res.send(응답 내용)
