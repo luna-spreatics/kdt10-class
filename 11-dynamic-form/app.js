10f8118a4986d060eb19812faf9355087687ce3c
luna-spr <luna@spreatics.com>
2023-11-27 14:01:30 +0900

    동적 폼 실습

--- 11-dynamic-form/app.js
+++ 11-dynamic-form/app.js
@@ -50,10 +50,37 @@ app.post('/fetch', (req, res) => {
     res.send({ name: req.body.name, gender: req.body.gender, msg: 'fetch 성공!' });
 })
 
-// 실습
+// ------------실습-----------------
 
 const userId = '홍길동';
 const userPw = '1234';
+
+app.get('/practice1', (req, res) => {
+    res.render('practice1')
+})
+
+app.get('/practice2', (req, res) => {
+    res.render('practice2')
+})
+
+app.get('/axios1', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/axios2', (req, res) => {
+    console.log(req.body); // { userId: 'banana', userPw: '1234' }
+
+    // userId, userPw 라는 변수 값과 클라이언트에서 넘겨받은 값이 일치하는지 검사
+    if (userId === req.body.userId && userPw === req.body.userPw) {
+        res.send({ userInfo: req.body, isSuccess: true });
+    } else {
+        res.send({ isSuccess: false });
+    }
+    // 결과 값을 반환
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`server is opening ${PORT}`);
