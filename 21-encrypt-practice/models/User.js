15fa5448a0beee23f6f047c265af8f7bd653ab5e
luna-spr <luna@spreatics.com>
2023-12-11 14:02:25 +0900

    비밀번호 암호화 실습

---
+++ 21-encrypt-practice/models/User.js
@@ -0,0 +1,34 @@
+// TODO: User 모델 정의
+const User = function (Sequelize, DataTypes) {
+    const model = Sequelize.define(
+        'user',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            pw: {
+                type: DataTypes.STRING(255),
+                allowNull: false
+            },
+            name: {
+                type: DataTypes.STRING(15),
+                allowNull: false
+            },
+            userid: {
+                type: DataTypes.STRING(15),
+                allowNull: false
+            }
+        },
+        {
+            freezeTableName: true,
+            timestamps: false
+        }
+    );
+
+    return model;
+}
+
+module.exports = User;
---
+++ 21-encrypt-practice/user.sql
@@ -0,0 +1,3 @@
+use kdt;
+show tables;
+select * from user;
