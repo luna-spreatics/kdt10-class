f401c74591f38516d3be10327b76850056ff90e7
luna-spr <luna@spreatics.com>
2024-01-05 14:03:53 +0900

    add map-filter practice code

--- 27-map-filter/src/App.js
+++ 27-map-filter/src/App.js
@@ -1,9 +1,12 @@
 import Alphabet from './Alphabet';
-
+import Ex3 from './Ex3';
 function App() {
   return (
     <div className="App">
-      <Alphabet />
+      {/* <Alphabet />
+      <hr /> */}
+
+      <Ex3 />
     </div>
   );
 }
