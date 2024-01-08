e2fdb7ec6c9c95058b29088c92a7cd6f420ec61f
luna-spr <luna@spreatics.com>
2024-01-08 14:11:35 +0900

    add hook, sass code

--- 30-hooks/src/App.js
+++ 30-hooks/src/App.js
@@ -1,5 +1,9 @@
+import Faq from './Faq';
+import Form from './Form';
+import ExForm from './ExForm';
 import UseCallbackEx from './UseCallbackEx';
 import UseMemoEx from './UseMemoEx';
+import UseReducerEx from './UseReducerEx';
 import UseCallbackEx2 from './useCallbackEx2';
 
 function App() {
@@ -10,6 +14,14 @@ function App() {
       <UseCallbackEx />
       <hr />
       <UseCallbackEx2 postId={3} />
+      <hr />
+      <UseReducerEx />
+      <hr />
+      <Faq />
+      <hr />
+      <Form />
+      <hr />
+      <ExForm />
     </div>
   );
 }
---
+++ 30-hooks/src/Faq.js
@@ -0,0 +1,17 @@
+import useToggle from './hooks/useToggle';
+
+function Faq() {
+  const [isFaqOpen, setIsFaqOpen] = useToggle();
+  console.log(useToggle());
+  return (
+    <>
+      <h1>custom hook (useToggle) ex</h1>
+      <div onClick={setIsFaqOpen} style={{ cursor: 'pointer' }}>
+        Q. 리액트에서 커스텀 훅 만들수 있나??
+      </div>
+      {isFaqOpen && <div>A. 가능합니다.</div>}
+    </>
+  );
+}
+
+export default Faq;
